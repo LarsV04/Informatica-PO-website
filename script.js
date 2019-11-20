2d4ddaf6a387b8c3a1884b784ef31c0cf224f15c
Larsie004 <lars.verkerk.pc@gmail.com>
2019-11-20 15:37:48 +0100

    Arrow

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
+var navright = true
 var navArrows = ['<svg><path d="M0 0 L10 10 L0 20"><path><svg>', '<svg><path d="M10 0 L0 10 L10 20"><path><svg>']
-var navright = false
 
 function SetNav() {
     document.getElementById("navbar").classList.toggle("extend")
-}
 
-function ChangeArrow() {
-    SetNav()
-
-    if (navright == false) {
-        document.getElementById("NavPijl").innerHTML = navArrows[1]
-        navright = true
-    } else if (navright == true) {
-        navright = false
-        document.getElementById("NavPijl").innerHTML = navArrows[0]
-    }
+    document.getElementById("NavPijl").classList.toggle("left")
 }
