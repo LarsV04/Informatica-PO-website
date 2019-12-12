c024ad27e3b5d02d1d64aba6818888780b0888b8
Larsie004 <lars.verkerk.pc@gmail.com>
2019-12-12 22:36:13 +0100

    Shit wat niet moet weg gehaald

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var navright = true
-var navArrows = ['<svg><path d="M0 0 L10 10 L0 20"><path><svg>', '<svg><path d="M10 0 L0 10 L10 20"><path><svg>']
 
 function SetNav() {
     document.getElementById("navbar").classList.toggle("extend")
